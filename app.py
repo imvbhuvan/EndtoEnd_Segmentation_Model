2f2ac36e929ac112c68993492775a51c0b19416e
Vaishak <mrvbhuvan@gmail.com>
2023-10-04 21:08:52 +0530

    Utils added with logger and Exceptions

---
+++ app.py
@@ -0,0 +1,6 @@
+from Cell_Segmentation.logger import logging
+from Cell_Segmentation.exception import AppException
+import sys 
+
+logging.info("Hello World - Welcome to the Cell Segmentation Project")
+
