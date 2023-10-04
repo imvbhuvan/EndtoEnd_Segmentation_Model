43fad0e737669a738914f37768542996ac3e07df
Vaishak <mrvbhuvan@gmail.com>
2023-10-04 18:59:37 +0530

    Installations performed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Vaishak Bhuvan M R',
+    author_email= 'mrvbhuvan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
